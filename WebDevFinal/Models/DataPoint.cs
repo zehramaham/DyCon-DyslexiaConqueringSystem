53ec9eacd0e996bf63cc7d9fa973db8a9090fbe7
zehramaham <91771431+zehramaham@users.noreply.github.com>
2021-10-02 13:35:55 +0500

    Add files via upload

---
+++ WebDevFinal/Models/DataPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WebDevFinal.Models
+{
+    //DataContract for Serializing Data - required to serve in JSON format
+    [DataContract]
+    public class DataPoint
+    {
+        public DataPoint(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "x")]
+        public Nullable<int> X = null;
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "y")]
+        public Nullable<int> Y = null;
+    }
+}
---
+++ WebDevFinal/Models/DataPoint2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WebDevFinal.Models
+{
+    //DataContract for Serializing Data - required to serve in JSON format
+    [DataContract]
+    public class DataPoint2
+    {
+        public DataPoint2(string label, double y)
+        {
+            this.Label = label;
+            this.Y = y;
+        }
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "label")]
+        public string Label = "";
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "y")]
+        public Nullable<double> Y = null;
+    }
+}
